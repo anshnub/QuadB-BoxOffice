0c60eb286244b50422c180614f0136f40828f397
ansh tyagi <anshtyagi1818@gmail.com>
2023-05-01 18:39:02 +0530

    cncted the api for searching the shows n movies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,38 @@
-
+import { useState } from "react";
 
 const Home = () => {
-    return <div>
-        HOME
+    const [searchStr,setSearchStr]=useState("")
+    
+
+    const onSearchInputChange = (ev) =>{
+        setSearchStr(ev.target.value);
+    }
+    
+    const onSearch =async (ev) =>{
+        ev.preventDefault();
+
+       const response= await fetch(`https://api.tvmaze.com/search/shows?q=${searchStr}`)
+       const body = await response.json()
+
+       console.log(body)
+       
+       
+
+       // https://api.tvmaze.com/search/shows?q=boys
+    };
+
+    return(
+         <div>
+
+    <form onSubmit={onSearch}>  
+
+    <input type="text" value={searchStr} onChange={onSearchInputChange} />
+
+    <button type="submit" >search here</button>
+    </form>
+    
     </div>
+    )
 }
 
 export default Home ;
